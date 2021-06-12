d7abc956e20b5aa15f2e0a3d9c91b770c156e7b8
yutodayo <50805424+yutodayo@users.noreply.github.com>
2021-06-12 19:17:22 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import { useCallback, useState, useMemo } from "react";
+import { ChildArea } from "./ChildArea";
+import "./styles.css";
+
+export default function App() {
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
+  const onChangeText = (e) => setText(e.target.value); //入力した値がsetTextの更新関数に入る
+
+  const onClickOpen = () => setOpen(!open);
+
+  const onClickClose = useCallback(() => setOpen(false), []);
+
+  const temp = useMemo(() => 1 + 3, []);
+  console.log(temp);
+
+  return (
+    <div className="App">
+      <input value={text} onChange={onChangeText} />
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open} onClickClose={onClickClose} />
+    </div>
+  );
+}
---
+++ src/ChildArea.jsx
@@ -0,0 +1,22 @@
+import { memo } from "react"; //memoに囲われた部分はpropsが変更されない限り再レンダリングしない
+
+const style = {
+  width: "100%",
+  height: "200px",
+  backgroundColor: "khaki"
+};
+
+export const ChildArea = memo((props) => {
+  const { open, onClickClose } = props;
+
+  return (
+    <>
+      {open ? (
+        <div style={style}>
+          <p>子コンポーネント</p>
+          <button onClick={onClickClose}>閉じる</button>
+        </div>
+      ) : null}
+    </>
+  );
+});
